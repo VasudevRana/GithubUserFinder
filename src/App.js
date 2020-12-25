f294331e5eea0f0b436bc7b59fb145b9a6c7a981
VasudevRana <45121999+VasudevRana@users.noreply.github.com>
2020-12-25 21:48:02 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./style.css";
+import axios from "axios";
+const url = `https://api.github.com/users`;
+export default function App() {
+  let [s, setS] = React.useState("");
+  let [myresults, setMyresults] = React.useState([]);
+  React.useEffect(function(){
+    let b = []
+    if (localStorage.getItem("results")) {
+      b = JSON.parse(localStorage.getItem("results"));
+
+    }
+    
+    
+    setMyresults(b);
+    
+  },[])
+
+  const _search = e => {
+    
+    axios
+      .get(url + "/" + s)
+      .then(d => d.data)
+      .then(d => {
+        if (d) {
+          let b = []
+          if (localStorage.getItem("results")) {
+            b = JSON.parse(localStorage.getItem("results"));
+
+          }
+          b.push(d);
+          localStorage.setItem("results", JSON.stringify(b));
+          setMyresults(b);
+          setS('');
+          alert("user added");
+        }
+        else{
+          alert("not found")
+        }
+      });
+  };
+  const del = index => {
+    let b = []
+    if (localStorage.getItem("results")) {
+      b = JSON.parse(localStorage.getItem("results"));
+
+    }
+    alert("Confirm delete");
+    b.splice(index, 1)
+    localStorage.setItem("results", JSON.stringify(b));
+    setMyresults(b);
+  }
+  return (
+    <div className="body">
+      <h1>Github Search</h1>
+
+     
+      <h4>To get The answer after writing username get out to input box,New Name wil be added in downwards direction</h4>
+      <input
+      className="input"
+        onChange={e => {setS(e.target.value);}
+        }
+        placeholder="search..."
+        value={s}
+      />
+      <button className="button" onMouseEnter={_search}  >search </button>
+      <div className="pre">
+        {myresults.map((x, i) => (
+          <li key={i}>
+            UserId:-{x.id} <br></br>
+            Name:-{x.name} <br></br>
+            Link:-{x.html_url} <br></br>
+            Repos:-{x.public_repos} <br></br>
+            followers:-{x.followers} <br></br>
+            following:-{x.following}<br></br>
+            <button className="delete" onClick={e => del(i)}>delete</button>
+          </li>
+      
